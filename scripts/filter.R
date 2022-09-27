5162d60191c38e0a17bfa60fe37a470b170cd3ae
Gabe Dragg <gabriel.dragg@selu.edu>
2022-09-27 19:58:57 +0000

    Added function to count butterfly species

--- scripts/filter.R
+++ scripts/filter.R
@@ -7,3 +7,10 @@ filter_years <- function(dataset = data){
     filter(years > 1997)
   return(new_data)
 }
+
+number_species <- function(dataset = data){
+  species_count <- data %>% 
+    group_by(ButterflySpecies) %>% 
+    count(ButterflySpecies)
+  return(species_count)
+}
