7e3e9bfedee7e0a84a83734bd38d1c3b39cf7311
Gabe Dragg <gabriel.dragg@selu.edu>
2022-10-10 02:29:46 +0000

    re added 2nd function

--- scripts/filter.R
+++ scripts/filter.R
@@ -6,3 +6,10 @@ filter_years <- function(dataset = data){
   new_data <- data %>% 
   return(new_data)
 }
+
+number_species <- function(dataset = data){
+  species_count <- data %>% 
+    group_by(ButterflySpecies) %>% 
+    count(ButterflySpecies)
+  return(species_count)
+}
